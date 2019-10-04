eb0d4e9ac319c77e2b604a6c108abc9af9283c0a
Wesley Karle <47843118+wesleykarle@users.noreply.github.com>
2019-10-03 19:48:12 -0700

    Add files via upload

---
+++ bool_Palindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace bool_Palindrome
+{
+    class Program
+    {
+        public static bool Palindrome(string input)
+        {
+            bool flag = false;
+            if (input == "")
+            {
+                Console.WriteLine("Not a valid input");
+            }
+            for (int i = 0, j = input.Length - 1; i < input.Length / 2; i++, j--)
+            {
+                if (input[i] != input[j])
+                {
+                    flag = false;
+                }
+                else
+                {
+                    flag = true;
+                }
+            }
+            return flag;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.Palindrome("Level"));
+        }
+    }
+}
